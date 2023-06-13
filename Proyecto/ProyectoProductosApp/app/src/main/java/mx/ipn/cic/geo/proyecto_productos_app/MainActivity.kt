3cfaca34a4aa388379635f960df64de8a8df07b8
ReinaLeonel <reina.zirtaeb@gmail.com>
2023-06-12 23:48:54 -0600

    Vistas Login y Signup, terminadas

--- Proyecto/ProyectoProductosApp/app/src/main/java/mx/ipn/cic/geo/proyecto_productos_app/MainActivity.kt
+++ Proyecto/ProyectoProductosApp/app/src/main/java/mx/ipn/cic/geo/proyecto_productos_app/MainActivity.kt
@@ -24,14 +24,17 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
+        // Quitar el ActionBar.
+        this.supportActionBar?.hide()
+
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
+        /*binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
-        }
+        }*/
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
